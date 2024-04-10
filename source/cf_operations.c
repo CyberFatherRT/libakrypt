e51537a810c431c6df6899c9ed96c7f8e49de246
CyberFather <maks12ate@gmail.com>
2024-04-10 12:30:57 +0300

    da

--- source/cf_operations.c
+++ source/cf_operations.c
@@ -13,9 +13,9 @@ typedef enum {
 } Mode;
 
 typedef struct {
-    char* algorithm;
-    char* input;
-    char* output;
+    char *algorithm;
+    char *input;
+    char *output;
     ak_uint8 *key;
     size_t key_size;
     ak_uint8 *iv;
@@ -23,13 +23,13 @@ typedef struct {
     Mode mode;
 } Config;
 
-int akrypt_encrypt(const Config* config) {
+int akrypt_encrypt(const Config *config) {
 
     struct bckey ctx;
 
-    char* algorithm = config->algorithm;
-    char* output = config->output;
-    char* input = config->input;
+    char *algorithm = config->algorithm;
+    char *output = config->output;
+    char *input = config->input;
     ak_uint8 *key = config->key;
     size_t key_size = config->key_size;
     ak_uint8 *iv = config->iv;
@@ -48,7 +48,7 @@ int akrypt_encrypt(const Config* config) {
     } else if (strcmp(algorithm, "magma") == 0) {
         ak_bckey_create_magma(&ctx);
     } else {
-        char* err_msg = malloc((20 + strlen(algorithm)) * sizeof(char));
+        char *err_msg = malloc((20 + strlen(algorithm)) * sizeof(char));
         sprintf(err_msg, "Unknown algorithm: %s", algorithm);
         ak_log_set_message(err_msg);
         free(err_msg);
@@ -58,48 +58,55 @@ int akrypt_encrypt(const Config* config) {
     ak_bckey_set_key(&ctx, key, key_size);
 
     switch (mode) {
-        case m_cbc:
-            error = ak_bckey_encrypt_cbc(&ctx, input, output, strlen(input), iv, iv_size);
-            if (error != ak_error_ok) goto exlab;
-            break;
-        case m_ctr:
-            error = ak_bckey_ctr(&ctx, input, output, strlen(input), iv, iv_size);
-            if (error != ak_error_ok) goto exlab;
-            break;
-        case m_ofb:
-            error = ak_bckey_ofb(&ctx, input, output, strlen(input), iv, iv_size);
-            if (error != ak_error_ok) goto exlab;
-            break;
-        case m_cfb:
-            error = ak_bckey_encrypt_cfb(&ctx, input, output, strlen(input), iv, iv_size);
-            if (error != ak_error_ok) goto exlab;
-            break;
-        case m_ecb:
-            error = ak_bckey_encrypt_ecb(&ctx, input, output, strlen(input));
-            if (error != ak_error_ok) goto exlab;
-            break;
+    case m_cbc:
+        error = ak_bckey_encrypt_cbc(&ctx, input, output, strlen(input), iv,
+                                     iv_size);
+        if (error != ak_error_ok)
+            goto exlab;
+        break;
+    case m_ctr:
+        error = ak_bckey_ctr(&ctx, input, output, strlen(input), iv, iv_size);
+        if (error != ak_error_ok)
+            goto exlab;
+        break;
+    case m_ofb:
+        error = ak_bckey_ofb(&ctx, input, output, strlen(input), iv, iv_size);
+        if (error != ak_error_ok)
+            goto exlab;
+        break;
+    case m_cfb:
+        error = ak_bckey_encrypt_cfb(&ctx, input, output, strlen(input), iv,
+                                     iv_size);
+        if (error != ak_error_ok)
+            goto exlab;
+        break;
+    case m_ecb:
+        error = ak_bckey_encrypt_ecb(&ctx, input, output, strlen(input));
+        if (error != ak_error_ok)
+            goto exlab;
+        break;
     }
-
 
     strcpy(output, ak_ptr_to_hexstr(output, strlen(input), ak_false));
 
-exlab: ak_bckey_destroy(&ctx);
+exlab:
+    ak_bckey_destroy(&ctx);
 
-    if (error == ak_error_ok) exitstatus = EXIT_SUCCESS;
+    if (error == ak_error_ok)
+        exitstatus = EXIT_SUCCESS;
 
     ak_libakrypt_destroy();
 
     return exitstatus;
 }
 
-
-int akrypt_decrypt(const Config* config) {
+int akrypt_decrypt(const Config *config) {
 
     struct bckey ctx;
 
-    char* algorithm = config->algorithm;
-    char* output = config->output;
-    char* input = config->input;
+    char *algorithm = config->algorithm;
+    char *output = config->output;
+    char *input = config->input;
     ak_uint8 *key = config->key;
     size_t key_size = config->key_size;
     ak_uint8 *iv = config->iv;
@@ -118,36 +125,39 @@ int akrypt_decrypt(const Config* config) {
     } else if (strcmp(algorithm, "magma") == 0) {
         ak_bckey_create_magma(&ctx);
     } else {
-        char* err_msg = malloc((20 + strlen(algorithm)) * sizeof(char));
+        char *err_msg = malloc((20 + strlen(algorithm)) * sizeof(char));
         sprintf(err_msg, "Unknown algorithm: %s", algorithm);
         ak_log_set_message(err_msg);
         free(err_msg);
-        return -1;
+        return 1;
     }
 
     ak_bckey_set_key(&ctx, key, key_size);
 
     switch (mode) {
-        case m_cbc:
-            error = ak_bckey_decrypt_cbc(&ctx, input, output, strlen(input), iv, iv_size);
-            break;
-        case m_ctr:
-            error = ak_bckey_ctr(&ctx, input, output, strlen(input), iv, iv_size);
-            break;
-        case m_ofb:
-            error = ak_bckey_ofb(&ctx, input, output, strlen(input), iv, iv_size);
-            break;
-        case m_cfb:
-            error = ak_bckey_decrypt_cfb(&ctx, input, output, strlen(input), iv, iv_size);
-            break;
-        case m_ecb:
-            error = ak_bckey_decrypt_ecb(&ctx, input, output, strlen(input));
-            break;
+    case m_cbc:
+        error = ak_bckey_decrypt_cbc(&ctx, input, output, strlen(input), iv,
+                                     iv_size);
+        break;
+    case m_ctr:
+        error = ak_bckey_ctr(&ctx, input, output, strlen(input), iv, iv_size);
+        break;
+    case m_ofb:
+        error = ak_bckey_ofb(&ctx, input, output, strlen(input), iv, iv_size);
+        break;
+    case m_cfb:
+        error = ak_bckey_decrypt_cfb(&ctx, input, output, strlen(input), iv,
+                                     iv_size);
+        break;
+    case m_ecb:
+        error = ak_bckey_decrypt_ecb(&ctx, input, output, strlen(input));
+        break;
     }
 
     ak_bckey_destroy(&ctx);
 
-    if (error == ak_error_ok) exitstatus = EXIT_SUCCESS;
+    if (error == ak_error_ok)
+        exitstatus = EXIT_SUCCESS;
 
     ak_libakrypt_destroy();
 
